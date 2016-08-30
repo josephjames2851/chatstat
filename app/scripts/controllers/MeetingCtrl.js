4455520765252be5625076d21e371db03f17a234
Joe Weinzettel <josephjames2851@gmail.com>
2016-08-30 15:43:30 -0500

    Add meeting view by id with messages view

--- app/scripts/controllers/MeetingCtrl.js
+++ app/scripts/controllers/MeetingCtrl.js
@@ -1,21 +1,44 @@
 (function() {
-    function MeetingCtrl(Room, $stateParams) {
+    function MeetingCtrl(Room, $stateParams, $firebaseArray) {
         this.roomId = $stateParams.meetingId;
+
+        var ref = firebase.database().ref().child("messages").orderByChild("roomId").equalTo(this.roomId);
+        this.messages = $firebaseArray(ref);
+        
+        function createRoom(name) {
+            console.log(name);
+        }
+        
         // use the room id to request the room data from the Room service
         this.room = Room.getRoomById($stateParams.meetingId);
-        console.log(this.room);
-        console.log("Hello from the meeting controller");
         
-        this.messages = [
-            {
-                user: "Kevin",
-                message: "Hey!"
-            }
-        ];
+        this.addMessage = function() {
+            this.messages.$add({
+                user: "Joe",
+                message: this.message,
+                roomId: this.roomId
+            }).then(function() {
+                console.log("it worked!");
+                this.message = "";
+            });
+//            this.messages.push({ user: "Joe", message: this.message });
+        
+        }
+        
+//        this.messages = [
+//            {
+//                user: "Kevin",
+//                message: "Hey!"
+//            },
+//            {
+//                user: "Joe",
+//                message: "Hello!"
+//            }
+//        ];
 
     }
     
     angular
         .module('chatstat')
-        .controller('MeetingCtrl', ['Room', '$stateParams', MeetingCtrl]);
+        .controller('MeetingCtrl', ['Room', '$stateParams', '$firebaseArray', MeetingCtrl]);
 })();
