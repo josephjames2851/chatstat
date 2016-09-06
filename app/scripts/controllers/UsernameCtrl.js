c710beb1e044753c91d5feb4d201f9fa93fedb83
Joe Weinzettel <josephjames2851@gmail.com>
2016-09-06 09:55:47 -0500

    Set username modal open and close now working

--- app/scripts/controllers/UsernameCtrl.js
+++ app/scripts/controllers/UsernameCtrl.js
@@ -1,22 +1,22 @@
 (function() {
-    function UsernameCtrl($scope, $cookies, Room, Meetings) {
+    function UsernameCtrl($scope, $cookies, $uibModalInstance, Room, Meetings) {
         console.log("Hello from the username controller");
         
         this.setUsername = function() {
             console.log($scope.username);
             $cookies.put("chatStatCurrentUser", $scope.username);
+            $uibModalInstance.close('username');
         }
         
     }
+//    
+//        $scope.setUsername = function () {
+//            $uibModalInstance.close('username');
+//        };
     
-    function ModalInstanceCtrl($scope, $cookies, $modalInstance) {
-        $scope.setUsername = function () {
-            $modalInstance.close('username');
-        };
-    }
     
     angular
         .module('chatstat')
-        .controller('UsernameCtrl', ['$scope', '$cookies', 'Room', 'Meetings', UsernameCtrl]);
+        .controller('UsernameCtrl', ['$scope', '$cookies', '$uibModalInstance', 'Room', 'Meetings', UsernameCtrl]);
         
 })();
